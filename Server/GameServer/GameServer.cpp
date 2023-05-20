8b27e15cc5e84efd57471a42b4aad523673269ed
Dowan <shukang91@gmail.com>
2023-05-21 00:25:24 +0900

    thread

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -2,7 +2,32 @@
 #include <iostream>
 #include "CorePch.h"
 
+#include <thread>
+
+void HelloThread()
+{
+	cout << "Hello Thread" << endl;
+}
+
 int main()
 {
-	HelloWorld();
+	std::thread t;
+
+	auto id1 = t.get_id();
+	
+	t = std::thread(HelloThread);
+
+	int32 count = t.hardware_concurrency(); // CPU 코어 개수?
+	
+	auto id2 = t.get_id(); // 쓰레드마다 id
+
+	//t.detach(); // std::thread 객체에서 실제 쓰레드를 분리
+
+	t.joinable(); // 객체가 살아있는지
+
+	if (t.joinable())
+	{
+		t.join();
+	}
+	
 }
