057f209fda612714e9f29f0235890363ce32f3c7
Dowan <shukang91@gmail.com>
2023-05-23 22:16:49 +0900

    condition variable

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -11,18 +11,25 @@ mutex m;
 queue<int32> q;
 HANDLE handle;
 
+// 참고) CV는 User-Level Object
+condition_variable cv;
+
+// condition_variable_any cv;
+
 void Producer()
 {
 	while (true)
 	{
+		// 1) Lock을 잡고, 2) 공유 변수 값을 수정
+		// 3) Lock을 풀고, 4) 조건변수 통해 다른 쓰레드에게 통지
+
 		{
 			unique_lock<mutex> lock(m);
 			q.push(100);
 		}
 
-		SetEvent(handle);
+		cv.notify_one(); // wait중인 쓰레드가 있으면 1개만 깨운다
 
-		this_thread::sleep_for(10000ms);
 	}
 }
 
@@ -30,17 +37,19 @@ void Consumer()
 {
 	while (true)
 	{
-		WaitForSingleObject(handle, INFINITE);
-		// Auto -> Non-Signal
-		//ResetEvent(handle) -> Manual
-
 		unique_lock<mutex> lock(m);
+		cv.wait(lock, []() {return q.empty() == false; });
+		// 1) Lock을 잡고, 2) 조건 확인
+		// ~ 만족 -> 빠져나와서 이어서 코드 진행
+		// ~ 만족x -> Lock을 풀어주고 대기 상태 -> lock_guard(x), unique_lock(o)
 
-		if (q.empty() == false)
+		// notify_one을 했으면 항상 조건식을 만족하는거 아닐까?
+		// notify_one -> 얘가 락을 잡기전에 공유변수를 다른애가 건들수 있음
+		
 		{
 			int32 data = q.front();
 			q.pop();
-			cout << data << endl;
+			cout << q.size() << endl;
 		}
 	}
 }
@@ -48,13 +57,6 @@ void Consumer()
 
 int main()
 {
-	// 커널 오브젝트
-	// Usage Count
-	// Signal , Non-Signal -> bool
-	// Auto, Manual -> bool
-
-	handle = CreateEvent(nullptr/*보안속성*/, false, false, nullptr);
-
 
 	thread t1(Producer);
 	thread t2(Consumer);
@@ -62,5 +64,4 @@ int main()
 	t1.join();
 	t2.join();
 
-	CloseHandle(handle);
 }
