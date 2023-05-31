4c857445a7cbe4a4d07e8096c3242e93cc492786
Dowan <shukang91@gmail.com>
2023-05-31 21:50:45 +0900

    Lock Free Stack 2

--- Server/GameServer/ConcurrentStack.h
+++ Server/GameServer/ConcurrentStack.h
@@ -64,18 +64,6 @@ public:
 	{
 		Node* node = new Node(value);
 		node->next = _head;
-
-
-		/*if (_head == node->next)
-		{
-			_head = node;
-			return true;
-		}
-		else
-		{
-			node->next = _head;
-			return false;
-		}*/
 		
 		while (_head.compare_exchange_weak(node->next, node) == false)
 		{
@@ -85,18 +73,9 @@ public:
 
 	bool TryPop(T& value)
 	{
-		Node* oldHead = _head;
+		++_popCount;
 
-		/*if (_head == node->next)
-		{
-			_head = oldHead->next;
-			return true;
-		}
-		else
-		{
-			oldHead = _head;
-			return false;
-		}*/
+		Node* oldHead = _head;
 
 		while (oldHead && _head.compare_exchange_weak(oldHead, oldHead->next) == false)
 		{
@@ -104,19 +83,90 @@ public:
 		}
 
 		if (oldHead == nullptr)
+		{
+			--_popCount;
 			return false;
+		}
 
 		value = oldHead->data;
 
 		// 잠시 삭제 보류
 		//delete oldHead;
-		
+
+		TryDelete(oldHead);
 		return true;
+	}
+
+	// 1) 데이터 분리
+	// 2) Count 체크
+	// 3) 나 혼자면 삭제
+	void TryDelete(Node* oldHead)
+	{
+		// 나 외에 누가 있는가?
+		if (_popCount == 1)
+		{
+			Node* node = _pendingList.exchange(nullptr);
+
+			if (--_popCount == 0)
+			{
+				// 끼어든 애가 없음 -> 분리한 데이터 삭제
+				DeleteNodes(node);
+			}
+			else if (node)
+			{
+				// 끼어든 애가 있음 -> 다시 갖다놓기
+				ChainPendingNodeList(node);
+			}
+
+			// 내 데이터 삭제
+			delete oldHead;
+		}
+		else
+		{
+			// 누가 있음
+			ChainPendingNode(oldHead);
+			--_popCount;
+		}
+	}
+
+	void ChainPendingNodeList(Node* first, Node* last)
+	{
+		last->next = _pendingList;
+
+		while (_pendingList.compare_exchange_weak(last->next, first) == false)
+		{
+		}
+	}
+
+	void ChainPendingNodeList(Node* node)
+	{
+		Node* last = node;
+		while (last->next)
+			last = last->next;
+
+		ChainPendingNodeList(node, last);
+	}
+
+	void ChainPendingNode(Node* node)
+	{
+		ChainPendingNodeList(node, node);
+	}
+
+	static void DeleteNodes(Node* node)
+	{
+		while (node)
+		{
+			Node* next = node->next;
+			delete node;
+			node = next;
+		}
 	}
 
 private:
 
-
-
 	atomic<Node*> _head;
+
+	atomic<uint32> _popCount = 0; // pop을 실행중인 쓰레드 개수
+	atomic<Node*> _pendingList; // 삭제 되어야 할 노드들
+
 };
