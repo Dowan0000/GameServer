78a7505e77f0b7334cb2dcb19967c03b15ad95df
Dowan <shukang91@gmail.com>
2023-06-06 22:24:03 +0900

    socket programming basics

--- Server/DummyClient/DummyClient.cpp
+++ Server/DummyClient/DummyClient.cpp
@@ -1,7 +1,48 @@
 ﻿#include "pch.h"
 #include <iostream>
 
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
 int main()
 {
-	
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket == INVALID_SOCKET)
+	{
+		int32 errCode = WSAGetLastError();
+		cout << "socket error: " << errCode << endl;
+		return 0;
+	}
+
+	SOCKADDR_IN serverAddr;
+	memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	//serverAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1"); // 구식 deprecated
+	inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);      // 이걸 쓰자
+	serverAddr.sin_port = htons(7777); // host to network short
+
+	if (connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		int32 errCode = WSAGetLastError();
+		cout << "connect error: " << errCode << endl;
+		return 0;
+	}
+
+	cout << "Connected to server" << endl;
+
+	while (true)
+	{
+		this_thread::sleep_for(1s);
+	}
+
+	// 소켓 반환
+	closesocket(clientSocket);
+	// 윈속 종료
+	WSACleanup();
 }
