7c79a25b5e9fccc34cc784baec70f777b6b969b2
Dowan <shukang91@gmail.com>
2023-06-08 22:17:18 +0900

    socket option

--- Server/DummyClient/DummyClient.cpp
+++ Server/DummyClient/DummyClient.cpp
@@ -25,65 +25,10 @@ int main()
 		return 0;
 	}
 
-	SOCKADDR_IN serverAddr;
-	memset(&serverAddr, 0, sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	//serverAddr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1"); // 구식 deprecated
-	inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);      // 이걸 쓰자
-	serverAddr.sin_port = htons(7777); // host to network short
-
-	// Connected UDP
-	connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
-	// send로 보냄
-
-	// ----------------------------
+	
 
 
-	while (true)
-	{
-		/*for (int32 i = 0; i < 10; i++)
-		{
-			char sendBuffer[100] = "Hello World!";
 
-			// 나의 IP주소 + 포트번호 설정
-			int resultCode = sendto(clientSocket, sendBuffer, sizeof(sendBuffer), 0, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
-			if (resultCode == SOCKET_ERROR)
-			{
-				int32 errCode = WSAGetLastError();
-				cout << "send error: " << errCode << endl;
-				return 0;
-			}
-
-			cout << "Send Data! Length: " << sizeof(sendBuffer) << endl;
-		}*/
-		
-		char sendBuffer[100] = "Hello World!";
-
-		// Unconnected UDP
-		// int resultCode = sendto(clientSocket, sendBuffer, sizeof(sendBuffer), 0, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
-		
-		// Connected UDP
-		int resultCode = send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
-
-		SOCKADDR_IN recvAddr;
-		memset(&recvAddr, 0, sizeof(recvAddr));
-		int recvAddrSize = sizeof(recvAddr);
-
-
-		char recvBuffer[1024] = {};
-		//int32 recvLen = recvfrom(clientSocket, recvBuffer, sizeof(recvBuffer), 0, (SOCKADDR*)&recvAddr, &recvAddrSize);
-		int32 recvLen = recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-		if (recvLen <= 0)
-		{
-			HandleError("RecvFrom");
-			return 0;
-		}
-
-		cout << "Recv Data! Data: " << recvBuffer << endl;
-		cout << "Recv Data! Length: " << recvLen << endl;
-
-		this_thread::sleep_for(1s);
-	}
 
 	// 소켓 반환
 	closesocket(clientSocket);
--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -31,46 +31,48 @@ int main()
 		return 0;
 	}
 
-	SOCKADDR_IN serverAddr;
-	memset(&serverAddr, 0, sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY); // host to network long
-	serverAddr.sin_port = htons(7777);
+	// SO_Keeplive = 주기적으로 연결 상태 확인 (TCP Only)
+	
+	bool enable = true;
+	setsockopt(serverSocket, SOL_SOCKET, SO_KEEPALIVE, (char*)&enable, sizeof(enable));
+	
+	// SO_Linger = 소켓 종료시 남아있는 데이터를 전송할 것인지
+	// onoff = 0이면 바로 closesocket return, 아니면 linger초만큼 대기
+	LINGER linger;
+	linger.l_onoff = 1;
+	linger.l_linger = 5;
+	setsockopt(serverSocket, SOL_SOCKET, SO_LINGER, (char*)&linger, sizeof(linger));
 
-	if (::bind(serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	// Half-close = shutdown()
+	// SO_SEND = send만 막음
+	// SO_RECV = recv만 막음
+	// SO_BOTH = send, recv 모두 막음
+	//::shutdown(serverSocket, SD_SEND);
+
+	// SO_SNDBUF = 송신 버퍼 크기
+	// SO_RCVBUF = 수신 버퍼 크기
+	int32 sendBufSize;
+	int32 optLen = sizeof(sendBufSize);
+	getsockopt(serverSocket, SOL_SOCKET, SO_SNDBUF, (char*)&sendBufSize, &optLen);
+	cout << "SO_SNDBUF: " << sendBufSize << endl;
+
+	int32 recvBufSize;
+	optLen = sizeof(recvBufSize);
+	getsockopt(serverSocket, SOL_SOCKET, SO_RCVBUF, (char*)&recvBufSize, &optLen);
+	cout << "SO_RCVBUF: " << recvBufSize << endl;
+
+	// SO_REUSEADDR = IP주소, 포트 재사용
 	{
-		HandleError("Bind");
-		return 0;
+		bool enable = true;
+		setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&enable, sizeof(enable));
 	}
 
-	while (true)
+	// IPPROTO_TCP = TCP
+	// TCP_NODELAY = Nagle 알고리즘 작동 여부
+	// 데이터가 충분히 크면 보내고, 그렇지않으면 쌓일때 까지 대기
 	{
-		char recvBuffer[1024] = {};
-
-		SOCKADDR_IN clientAddr;
-		memset(&clientAddr, 0, sizeof(clientAddr));
-		int clientAddrSize = sizeof(clientAddr);
-
-		this_thread::sleep_for(1s);
-
-		int32 recvLen = recvfrom(serverSocket, recvBuffer, sizeof(recvBuffer), 0, (SOCKADDR*)&clientAddr, &clientAddrSize);
-		if (recvLen <= 0)
-		{
-			HandleError("RecvFrom");
-			return 0;
-		}
-
-		cout << "Recv Data! Data = " << recvBuffer << endl;
-		cout << "Recv Data! Length = " << recvLen << endl;
-
-		int32 errCode = sendto(serverSocket, recvBuffer, recvLen, 0, (SOCKADDR*)&clientAddr, sizeof(clientAddr));
-		if (errCode == SOCKET_ERROR)
-		{
-			HandleError("SendTo");
-			return 0;
-		}
-
-		cout << "Send Data! Len = " << recvLen << endl;
+		bool enable = true;   // NO_DELAY = true : Nagle 알고리즘 끔
+		setsockopt(serverSocket, IPPROTO_TCP, TCP_NODELAY, (char*)&enable, sizeof(enable));
 	}
 
 
