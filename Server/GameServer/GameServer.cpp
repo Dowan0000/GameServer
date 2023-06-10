7bbc095db2cee48632ac0da086cb5954b3b962ea
Dowan <shukang91@gmail.com>
2023-06-10 23:41:42 +0900

    select

--- Server/GameServer/GameServer.cpp
+++ Server/GameServer/GameServer.cpp
@@ -18,6 +18,16 @@ void HandleError(const char* msg)
 	cout << msg << " error: " << errCode << endl;
 }
 
+const int32 BUFSIZE = 1024;
+
+struct Session
+{
+	SOCKET socket = INVALID_SOCKET;
+	char recvBuffer[BUFSIZE] = { };
+	int32 recvBytes = 0;
+	int32 sendBytes = 0;
+};
+
 int main()
 {
 	WSADATA wsaData;
@@ -62,9 +72,88 @@ int main()
 		return 0;
 	}
 
-	
+	// select 모델
+	// 소켓 함수 호출이 성공할 시점을 미리 알 수 있다
 
+	// socket set 읽기, 쓰기, 예외 관찰
+	// select(readSet, writeSet, exceptSet, timeout) - 관찰 시작
+	// 적어도 하나의 소켓이 준비되면 리턴 - 낙오자는 알아서 제거됨
 
+	vector<Session> sessions;
+	sessions.reserve(100);
+
+	fd_set reads;
+	fd_set writes;
+
+	while (true)
+	{
+		FD_ZERO(&reads);
+		FD_ZERO(&writes);
+
+		FD_SET(listenSocket, &reads);
+
+		for (Session& s : sessions)
+		{
+			if(s.recvBytes <= s.sendBytes)
+				FD_SET(s.socket, &reads);
+			else
+				FD_SET(s.socket, &writes);
+		}
+
+		int32 retVal = select(0, &reads, &writes, nullptr, nullptr);
+		if (retVal == SOCKET_ERROR)
+		{
+			HandleError("select");
+			break;
+		}
+
+		// listner 소켓 체크
+		if (FD_ISSET(listenSocket, &reads))
+		{
+			SOCKADDR_IN clientAddr;
+			int32 addrLen = sizeof(clientAddr);
+			SOCKET clientSocket = accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+			if (clientSocket != INVALID_SOCKET)
+			{
+				cout << "Client Connected" << endl;
+				sessions.push_back(Session{ clientSocket });
+			}
+		}
+
+		// 나머지 소켓 체크
+		for (Session& s : sessions)
+		{
+			// read 체크
+			if (FD_ISSET(s.socket, &reads))
+			{
+				int32 recvLen = recv(s.socket, s.recvBuffer, BUFSIZE, 0);
+				if (recvLen <= 0)
+				{
+					// sessions 에서 제거
+					continue;
+				}
+
+				s.recvBytes = recvLen;
+			}
+
+			// write 체크
+			if (FD_ISSET(s.socket, &writes))
+			{
+				// 블로킹 모드 -> 모든 데이터 다 보내
+				// 논블로킹 모드 -> 일부만 보낼 수 있음
+				int32 sendLen = send(s.socket, &s.recvBuffer[s.sendBytes], s.recvBytes - s.sendBytes, 0);
+				if (sendLen <= 0)
+				{
+					// sessions 에서 제거
+					continue;
+				}
+
+				s.sendBytes += sendLen;
+				if (s.sendBytes == s.recvBytes)
+					s.sendBytes = s.recvBytes = 0;
+			}
+		}
+	}
 
 	
 
